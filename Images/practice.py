58b9a60f2a1b460cfa526d19222499be3e8c4f20
K Vaishnavi Bhat <kvaishnavi1902@gmail.com>
2024-07-15 18:19:18 +0530

    fashion-quiz

---
+++ Images/practice.py
@@ -0,0 +1,11 @@
+'''light = input("Enter the colour: ")
+if (light=="Red"):
+    print("Stop")
+elif (light=="Yellow"):
+    print("Get ready")
+elif (light=="Green"):
+    print("Go")
+else:
+    print("Invalid colour light")'''
+
+
